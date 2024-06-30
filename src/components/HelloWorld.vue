494252e8eda184841796af8810cdc713998f928e
Muhammed Adnan <147593005+muhammedadnanv@users.noreply.github.com>
2024-06-30 20:26:09 +0530

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,50 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    
+    <h1>My name is   Muhammed Adnan</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    I am a self leanered developer  
+
+      <a href="ttps://muhammedadnanavv.vercel.app">Visit Now</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    </div>
+    <div id="fuck">
+ <h3>  How Are You,  if you Are a Self learner you Can learn it And bulid Projects  </h3>
+    </div>
+<div id="Socialmedia">
+<h3>Follow me on Social Media</h3>
+   <button style="background-color: black; color: aquamarine;"><a href="https://github.com/muhammedadnanv">Github</a></button>
+    <br><br>
+  <button style="background-color: black; color: aquamarine;"><a href="https://www.facebook.com/profile.php?id=100084139741037">Facebook</a></button>
+    <br><br>
+    <button style="background-color: black; color: aquamarine;"><a href="https://www.linkedin.com/in/muhammedadnanvv/">Linkedin</a></button>
+    <br>
+<div>
+  <h1>Contact me </h1>
+  <div style="background-color: aquamarine; color:black; cursor: auto;" element-loading-text="u4">
+  
+  <form action="https://formbold.com/s/9XqPy" method="post">
+    <label for="text">Enter Name</label>
+    <br>
+<input type="text" name="Enter Your name ">
+<br>
+<label for="email">Enter Email</label>
+<br>
+<input type="email" name="Enter your email">
+<br>
+<br>
+<input style="background-color: black; color:greenyellow;" type="submit" value="summit">
+  </form>
+</div>
+</div>
+
+</div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+ export default{
+  name
+ }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
